04dc15c4888efe1d5d5e10f1da2261717cdeac49
Ryan Nam <alfofh7060@gmail.com>
2023-12-12 20:29:04 +1100

    Add Exercise 1 - responsive layout

--- mindarc-nam/src/main.js
+++ mindarc-nam/src/main.js
@@ -4,6 +4,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// Add bootstrap version 4.4 CDN
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 const app = createApp(App)
 
 app.use(router)
--- mindarc-nam/src/router/index.js
+++ mindarc-nam/src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ResponsiveLayout from '../views/ResponsiveLayout.vue';
-import DataTabsAccordion from '../views/ResponsiveLayout.vue';
+import DataTabsAccordion from '../views/DataTabsAccordion.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
