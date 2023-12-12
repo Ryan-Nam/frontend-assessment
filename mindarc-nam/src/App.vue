0b4f7f42c22ef66d85168b4df98a7f27876e8600
Ryan Nam <alfofh7060@gmail.com>
2023-12-12 20:04:45 +1100

    Update HomeView as a landing page

--- mindarc-nam/src/App.vue
+++ mindarc-nam/src/App.vue
@@ -1,78 +1,11 @@
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.webp" width="125" height="125" />
-    <h1>Welcome to the Frontend Assessment Project for Mindarc!</h1>
-    <nav>
-      <RouterLink to="/ResponsiveLayout">Go to Responsive Layout</RouterLink>
-      <RouterLink to="/DataTabsAccordion">View Data Tabs & Accordion</RouterLink>
-    </nav>
-    <div class="contact-info">
-      <p>Hyunwoo Nam (Ryan)</p>
-      <a href="mailto:ryannam.dev@gmail.com">ryannam.dev@gmail.com</a>
-    </div>
-  </header>
+  <router-view />
 </template>
 
 <script setup>
-import { RouterLink } from 'vue-router'
+
 </script>
 
 <style scoped>
-header {
-  text-align: center;
-  max-height: 100vh;
-  padding: 2rem 0;
-}
 
-.logo {
-  margin-bottom: 2rem;
-}
-
-nav {
-  font-size: 1rem;
-  margin-top: 2rem;
-}
-
-nav a {
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-  color: var(--color-mindarc);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-.contact-info {
-  margin-top: 2rem;
-  font-size: 0.9rem;
-}
-
-.contact-info a {
-  color: var(--color-text);
-  text-decoration: none;
-}
-
-.contact-info a:hover {
-  text-decoration: underline;
-}
-
-@media (min-width: 1024px) {
-  header {
-    padding-right: 0; /* Centered content */
-  }
-
-  nav {
-    text-align: center;
-    margin-left: 0;
-  }
-}
 </style>
--- mindarc-nam/src/views/DataTabsAccordion.vue
+++ mindarc-nam/src/views/DataTabsAccordion.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="about">
+    <div>
       <h1>This is an Accodion page</h1>
     </div>
   </template>
